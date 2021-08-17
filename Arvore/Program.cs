cbc787ba15e7fa94b7a58e18d1e16cdcfea66f24
Scalfi <gascalfi@gmail.com>
2021-08-16 22:58:28 -0300

    colocando frazes

--- Arvore/Program.cs
+++ Arvore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 
 
@@ -21,44 +22,97 @@ namespace Arvore
 
         static void Main(string[] args)
         {
-            int[] myIntArray = new int[6] { 3, 2, 1, 6, 0, 5 };
+            int[] array1 = new int[6] { 3, 2, 1, 6, 0, 5 };
+            int[] array2 = new int[7] { 7, 5, 13, 9, 1, 6, 4 };
 
-            var maxValue = myIntArray.Max();
-            int maxIndex = myIntArray.ToList().IndexOf(maxValue);
+            printArvoce(array1);
+
+            Task.Delay(1000);
+            printArvoce(array2);
+
+        }
+
+        public static void printArvoce(int[] array)
+        {
+            Console.WriteLine($"Array de entrada: [{string.Join(",", array)}]");
+
+            var maxValue = array.Max();
+            int maxIndex = array.ToList().IndexOf(maxValue);
+            Console.WriteLine($"Raiz:{maxValue}");
+
             // changing array to list to use linq
 
-            var arrayList = myIntArray.ToList();
-            var rightArray = arrayList.GetRange(0, maxIndex).OrderByDescending(a=> a).ToArray();
-            var leftArray  = arrayList.Skip(maxIndex+1).OrderByDescending(a => a).ToArray();
+            var arrayList = array.ToList();
+            var leftArray  = arrayList.GetRange(0, maxIndex).OrderByDescending(a => a).ToArray();
+            var rightArray = arrayList.Skip(maxIndex + 1).OrderByDescending(a => a).ToArray();
 
-            for (int i = 0; i < myIntArray.Length; i++)
+            Console.WriteLine($"Galhos da esquerda:{string.Join(",", leftArray)}");
+            Console.WriteLine($"Galhos da direita:{string.Join(",", rightArray)}");
+            Console.WriteLine("");
+
+
+
+            for (int i = 0; i < array.Length; i++)
             {
-                if (i==0)
+                if (i == 0)
                 {
-                    Console.Write("".PadRight(myIntArray.Length));
+                    Console.Write("".PadRight(array.Length));
                     Console.WriteLine($"{maxValue}");
-                }
-
-                if (i < rightArray.Length)
-                {
-                    Console.Write("".PadRight(myIntArray.Length - i - 1));
-                    Console.Write($"{rightArray[i]}");
                 }
 
                 if (i < leftArray.Length)
                 {
+                    Console.Write("".PadRight(array.Length - i - 1));
+                    Console.Write($"/");
 
-                    Console.Write("".PadLeft(i + 1 + leftArray.ToList().IndexOf(leftArray[i])));
-                    Console.Write($"{leftArray[i]}");
+                }
+
+                if (i < rightArray.Length)
+                {
+                    if (i < leftArray.Length)
+                    {
+                        Console.Write("".PadLeft(i + 1 + rightArray.ToList().IndexOf(rightArray[i])));
+                        Console.Write($"\\");
+
+                    }
+                    else
+                    {
+                        Console.Write("".PadRight(array.Length - i));
+                        Console.Write("".PadLeft(i + 1 + rightArray.ToList().IndexOf(rightArray[i])));
+                        Console.Write($"\\");
+                    }
 
                 }
 
                 Console.WriteLine("");
+
+
+                if (i < leftArray.Length)
+                {
+                    Console.Write("".PadRight(array.Length - i - 2));
+                    Console.Write($"{leftArray[i]}");
+
+                }
+
+                if (i < rightArray.Length)
+                {
+                    if( i < leftArray.Length)
+                    {
+                        Console.Write("".PadLeft(i + 2 + rightArray.ToList().IndexOf(rightArray[i])));
+                        Console.Write($"{rightArray[i]}");
+
+                    } else
+                    {
+                        Console.Write("".PadRight(array.Length - i));
+                        Console.Write("".PadLeft(i + 2 + rightArray.ToList().IndexOf(rightArray[i])));
+                        Console.Write($"{rightArray[i]}");
+                    }
+                   
+                }
+
+                Console.WriteLine("");
             }
-
         }
-
-     
-
+ 
     }
 }
